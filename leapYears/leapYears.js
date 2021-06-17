a6c3537fc6286e003361aed67b338ae5161b5195
Ryan <ryan3738@gmail.com>
2021-06-17 11:17:50 -0700

    leap years

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+    //Test if divisible by 400
+if (testYear%400 === 0){
+    return true
+}
+//Test if divisible by 100
+if (testYear%100 ===0 && testYear%4 ===0){
+    false
+}
+//Test if divisible by 4
+    if (testYear % 4 === 0 && testYear % 100 !== 0) {
+        return true
+    }
+return false
 };
 
 module.exports = leapYears;
