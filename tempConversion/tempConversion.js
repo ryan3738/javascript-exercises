01973f098ba3fc4cec62d28859d102eecb913b80
Ryan <ryan3738@gmail.com>
2021-06-17 11:42:59 -0700

    temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
+const ftoc = function(farenheit) {
+
+  let celcius = Math.round((farenheit - 32) * 5 / 9 * 10) / 10
+  return celcius
 
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  // [°F] = [°C] × 9⁄5 + 32
+  let farenheit = Math.round((celcius * 9 / 5 + 32)*10) /10
+return farenheit
 };
+
+console.log(ftoc(100));
+console.log(ctof(-10));
 
 module.exports = {
   ftoc,
