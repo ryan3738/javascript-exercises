596f6ab000a009c45328d133f14aa225bbfedc56
Ryan <ryan3738@gmail.com>
2021-06-21 15:55:43 -0700

    calculator tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x, y) {
+return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+return numberArray.reduce((a, b)=> a + b, 0)
 };
 
-const multiply = function() {
+const multiply = function (numberArray) {
+  return numberArray.reduce((a, b) => a * b, 1)
+};
+
+const power = function (x, y) {
+  return Math.pow(x, y)
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) return -1;
+  if (num === 0) return 1;
+  if (num > 0) return num * factorial(num-1)
 };
 
 module.exports = {
