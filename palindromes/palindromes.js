dd1447796935df92443443a6b286c74a444a452c
Ryan <ryan3738@gmail.com>
2021-06-21 16:11:14 -0700

    palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let regex = /[\W_]/g;
+    //create new variable to store string
+    let lowerRegexString = string.toLowerCase().replace(regex, '') ;
+    let reversedString = lowerRegexString.split('').reverse().join('');
+    console.log(reversedString)
+    //Remove any non letters
+
+    //convert all to lowercase
+    //split string into array
+    //reverse the array
+    //join array into string
+    //compare reversed string to original
+
+
+return lowerRegexString === reversedString
 };
+
 
 module.exports = palindromes;
